9f4b49017608dd1fb811760a73f321d01b43fce8
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-12-20 18:01:27 +0545

    Finding training and validation accuracy

--- ReviewClassifier.playground/Contents.swift
+++ ReviewClassifier.playground/Contents.swift
@@ -10,10 +10,22 @@ do{
     let testingDataTable = try MLDataTable(contentsOf: testingDataFileURL)
     let stats = """
 =============================================================
-Entries used for training : \(trainingDataTable.size)
-Entries used for training : \(testingDataTable.size)
+    Entries used for training : \(trainingDataTable.size)
+    Entries used for training : \(testingDataTable.size)
+=============================================================
 """
     print(stats)
+    let sentimentClassifier = try MLTextClassifier(trainingData: trainingDataTable, textColumn: "text", labelColumn: "label")
+    let trainingAccuracy = (1.0 - sentimentClassifier.trainingMetrics.classificationError) * 100
+    let validationAccuracy = (1.0 - sentimentClassifier.validationMetrics.classificationError) * 100
+    
+    let message = """
+=============================================================
+    Training Accuracy = \(trainingAccuracy)
+    Validation Accuracy = \(validationAccuracy)
+=============================================================
+"""
+    print(message)
 }catch{
     print(error.localizedDescription)
 }
