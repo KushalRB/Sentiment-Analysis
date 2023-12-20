e111a96ecd4b7552de80b8d8056ff49df8a7631a
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-12-20 17:18:50 +0545

    Loaded Training and Testing data

--- ReviewClassifier.playground/Contents.swift
+++ ReviewClassifier.playground/Contents.swift
@@ -1,2 +1,7 @@
 import CreateML
+import Cocoa
 
+guard let trainingDataFileURL = Bundle.main.url(forResource: "amazon-reviews", withExtension: "json"),
+      let testingDataFileURL = Bundle.main.url(forResource: "testing-reviews", withExtension: "json")else {
+          fatalError("Error! Could not load resource files")
+      }
