ebb0c86cbb9bbe4e0029db8a0e9e45c6b99f681a
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-12-20 17:40:19 +0545

    Converted JSON files into Data Tables

--- ReviewClassifier.playground/Contents.swift
+++ ReviewClassifier.playground/Contents.swift
@@ -5,3 +5,15 @@ guard let trainingDataFileURL = Bundle.main.url(forResource: "amazon-reviews", w
       let testingDataFileURL = Bundle.main.url(forResource: "testing-reviews", withExtension: "json")else {
           fatalError("Error! Could not load resource files")
       }
+do{
+    let trainingDataTable = try MLDataTable(contentsOf: trainingDataFileURL)
+    let testingDataTable = try MLDataTable(contentsOf: testingDataFileURL)
+    let stats = """
+=============================================================
+Entries used for training : \(trainingDataTable.size)
+Entries used for training : \(testingDataTable.size)
+"""
+    print(stats)
+}catch{
+    print(error.localizedDescription)
+}
