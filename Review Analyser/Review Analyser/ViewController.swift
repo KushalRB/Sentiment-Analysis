2df22a3f1cd3d064ef0dd21dabd8b14d6c84f20d
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-12-22 18:27:32 +0545

    Integrated Review Classifier Model

--- Review Analyser/Review Analyser/ViewController.swift
+++ Review Analyser/Review Analyser/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import NaturalLanguage
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var sentimentImageView: UIImageView!
     @IBOutlet weak var clearButton: UIButton!
     
-   // MARK: - IBActions
+   //MARK: - IBActions
+    @IBAction func onClearPressed(_ sender: Any) {
+        textView.text = ""
+        clearButton.isHidden = true
+    }
+    
+   //MARK: - Variables
+    private lazy var sentimentClassifier: NLModel? = {
+        let model = try? NLModel(mlModel: CreateML_ReviewClassifier().model)
+        return model
+    }()
     
     
     //MARK: - View Controller Life Cycles
@@ -24,5 +35,29 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+//MARK: - Extension
+extension ViewController : UITextViewDelegate{
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if !(textView.text.isEmpty), text == "\n"{
+            if let label = sentimentClassifier?.predictedLabel(for: textView.text){
+                switch label{
+                case "positive":
+                    sentimentImageView.image = UIImage(named: "positive")
+                case "negative":
+                    sentimentImageView.image = UIImage(named: "negative")
+                default:
+                    sentimentImageView.image = UIImage(named: "neutral")
+                }
+            }
+        }
+        textView.resignFirstResponder()
+        return true
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        clearButton.isHidden = textView.text.isEmpty
+    }
 }
 
